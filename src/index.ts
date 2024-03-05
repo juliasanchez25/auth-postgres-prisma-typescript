4079b2abeb25422f2bcb8db1fc157ddc54ecfb62
Julia Sanchez <2509juliasanchez@gmail.com>
2024-03-05 12:09:49 -0300

    feat: 🎸 login route and authentication

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from 'express'
+import express, { Request, Response, NextFunction} from 'express';
 import dotenv from 'dotenv'
+import jsonwebtoken from 'jsonwebtoken'
 import { prisma } from '../prisma'
 
 dotenv.config()
@@ -9,8 +10,54 @@ const port = 3000
 
 app.use(express.json());
 
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.header("Authorization")
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid authorization header" });
+  }
+
+  const token = authHeader.replace("Bearer ", "")
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Authorization token not found" });
+  }
+
+  try {
+    const decoded = jsonwebtoken.verify(token, (process.env as any).JWT_SECRET_KEY);
+    (req as any).user = decoded;
+    next();
+  } catch (err) {
+    console.error(err);
+    return res.status(401).json({ success: false, message: "Invalid token" });
+  }
+};
+
 app.get('/', async (req, res) => {
   res.send(await prisma.user.findMany())
+})
+
+app.get('/books', authMiddleware, (req, res) => {
+  try {
+    const books = [
+      {
+        title: "Percy Jackson",
+        author: "Rick Riordan"
+      },
+      {
+        title: "Scott Pilgrim",
+        author: "Bryan Lee O'Malley"
+      }
+    ]
+
+    res.send(books)
+  } catch (err) {
+    console.log('Can not find books')
+  }
 })
 
 app.post('/register', async (req, res) => {
@@ -35,23 +82,27 @@ app.post('/register', async (req, res) => {
   }
 })
 
-app.get('/books', (req, res) => {
-  try {
-    const books = [
-      {
-        title: "Percy Jackson",
-        author: "Rick Riordan"
-      },
-      {
-        title: "Scott Pilgrim",
-        author: "Bryan Lee O'Malley"
-      }
-    ]
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
 
-    res.send(books)
-  } catch (err) {
-    console.log('Can not find books')
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+      password
+    },
+  });
+
+  if (!user) {
+    return res
+      .status(401)
+      .json({ message: 'Invalid login' })
   }
+
+  const token = jsonwebtoken.sign({ userId: user.id }, process.env.JWT_SECRET as string, {
+    expiresIn: '1h',
+  });
+
+  return res.json({ user, token })
 })
 
 app.listen(port, () => console.log(`App is running on port ${port}`))
